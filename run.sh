f9458d8a1eace48e19fb71e15ccedb7e4e9acd73
David Wake <dwake@box.com>
2021-10-15 20:50:24 -0700

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+git checkout -f working_version
+kubectl delete validatingwebhookconfiguration grumpy
+kubectl delete pod smooth-app not-smooth-app
+kubectl delete secret grumpy
+kubectl delete deployment grumpy
+kubectl delete service grumpy
+kubectl delete pod -l name=grumpy
+./gen_certs.sh
+kubectl create secret generic grumpy -n default  --from-file=key.pem=certs/grumpy-key.pem  --from-file=cert.pem=certs/grumpy-crt.pem
+kubectl apply -f  manifest.yaml
+kubectl rollout status deployment grumpy
+echo "This should fail"
+kubectl apply -f app_wrong.yaml
+echo "This should succeed"
+kubectl apply -f app_ok.yaml 
